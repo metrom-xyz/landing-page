18c64f64473ea5bca89f1c3be670a47a908f9f5c
Paolo Guerra <pg.guerrap@proton.me>
2024-09-16 17:06:36 +0200

    fix: issues with robots and sitemap files

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,10 +1,14 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
+    const locales = ["en"];
+
+    const rules = locales.map((locale) => ({
+        userAgent: "*",
+        allow: `/${locale}/`,
+    }));
+
     return {
-        rules: {
-            userAgent: "*",
-            allow: "/",
-        },
+        rules,
     };
 }
