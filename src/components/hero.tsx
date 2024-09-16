1e2235f32bc6d18f1f10c2ccdba432b7f0033200
Paolo Guerra <pg.guerrap@proton.me>
2024-09-16 22:38:57 +0200

    fix: minor style fixes

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -9,9 +9,9 @@ export function Hero() {
     const t = useTranslations("hero");
 
     return (
-        <section className="w-full flex flex-col gap-9 items-center text-center justify-center max-w-screen-2xl py-28 px-4 rounded-4xl bg-white">
+        <section className="w-full flex flex-col gap-9 items-center text-center justify-center max-w-screen-2xl pt-28 pb-4 md:py-28 px-4 rounded-4xl bg-white">
             <Metrom className="mb-9" />
-            <Typography className="text-[2.5rem] md:text-6xl leading-normal font-semibold max-w-5xl">
+            <Typography className="text-[2.5rem] md:text-6xl leading-none md:leading-normal font-semibold max-w-5xl">
                 {t("title")}
             </Typography>
             <Typography
@@ -21,13 +21,13 @@ export function Hero() {
             >
                 {t("description")}
             </Typography>
-            <div className="flex gap-3">
+            <div className="flex w-full md:w-fit gap-3">
                 <Button
                     href={APP_LINK}
                     target="_blank"
                     rel="noopener noreferrer"
                     className={{
-                        root: "md:min-w-44 bg-brand-green hover:bg-green-300 text-black",
+                        root: "w-full md:min-w-44 bg-brand-green hover:bg-green-300 text-black",
                     }}
                 >
                     {t("launch")}
@@ -36,7 +36,7 @@ export function Hero() {
                     href={DOCUMENTATION_LINK}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className={{ root: "md:min-w-44" }}
+                    className={{ root: "w-full md:min-w-44" }}
                 >
                     {t("more")}
                 </Button>
