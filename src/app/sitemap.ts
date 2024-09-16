0bf9ffda806f5a7f2b96935bbe588cf5f070cd7e
Paolo Guerra <pg.guerrap@proton.me>
2024-09-16 17:06:36 +0200

    fix: issues with robots and sitemap files

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,11 +2,17 @@ import { BASE_URL } from "@/common";
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    return [
-        {
-            url: BASE_URL,
+    const locales = ["en"];
+
+    const urls: MetadataRoute.Sitemap = [];
+
+    locales.forEach((locale) => {
+        urls.push({
+            url: `${BASE_URL}/${locale}`,
             changeFrequency: "monthly",
             priority: 1,
-        },
-    ];
+        });
+    });
+
+    return urls;
 }
