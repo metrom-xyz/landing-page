71de861091a6632bcb861d44a8354b313ae77639
Paolo Guerra <pg.guerrap@proton.me>
2024-09-16 22:46:21 +0200

    feat: make article card clickable, add hover colors

--- src/components/articles/article.tsx
+++ src/components/articles/article.tsx
@@ -10,21 +10,23 @@ export function Article({ title, link }: ArticleProps) {
     const t = useTranslations("articles");
 
     return (
-        <div className="w-full md:w-fit min-h-[344px] md:min-h-[500px] max-w-[413px] flex flex-col items-center text-center gap-8 p-10 bg-white rounded-2xl">
-            <Typography
-                variant="lg"
-                uppercase
-                weight="medium"
-                className="text-gray-600"
-            >
-                {t("title")}
-            </Typography>
-            <Typography className="text-4xl md:text-xl5 font-semibold flex-1">
-                {title}
-            </Typography>
-            <Button href={link} target="_blank" rel="noopener noreferrer">
-                {t("read")}
-            </Button>
-        </div>
+        <a href={link} target="_blank" rel="noopener noreferrer">
+            <div className="w-full md:w-fit min-h-[344px] md:min-h-[500px] max-w-[413px] flex flex-col items-center text-center gap-8 p-10 bg-white rounded-2xl duration-200 ease-in-out transition-colors hover:bg-gray-200">
+                <Typography
+                    variant="lg"
+                    uppercase
+                    weight="medium"
+                    className="text-gray-600"
+                >
+                    {t("title")}
+                </Typography>
+                <Typography className="text-4xl md:text-xl5 font-semibold flex-1">
+                    {title}
+                </Typography>
+                <Button className={{ root: "pointer-events-none" }}>
+                    {t("read")}
+                </Button>
+            </div>
+        </a>
     );
 }
