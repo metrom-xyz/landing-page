a295c122c6f0c496801699aa4937b7a7cb859559
Paolo Guerra <pg.guerrap@proton.me>
2024-12-19 15:19:58 +0100

    feat(frontend): remove swapsicle and baseswap

--- src/app/assets/logos/baseswap.tsx
+++
@@ -1,7 +0,0 @@
-import Image from "next/image";
-import baseSwapLogo from "./baseswap.webp";
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function BaseSwap(props: any) {
-    return <Image {...props} src={baseSwapLogo} alt="BaseSwap logo" />;
-}
