cbcb8084a306e3020f0fcc1458557e655229ed43
Paolo Guerra <pg.guerrap@proton.me>
2025-06-23 17:26:06 +0200

    feat: reduce article illustrations opacity, upgrade dependencies

--- src/components/home/articles/article.tsx
+++ src/components/home/articles/article.tsx
@@ -20,7 +20,7 @@ export function Article({
     return (
         <div className="relative overflow-hidden theme-surface w-full md:w-fit min-h-[344px] md:min-h-[500px] max-w-[413px] flex flex-col items-center text-center gap-8 p-10 rounded-2xl">
             <div className="absolute -bottom-16 md:bottom-0 md:h-auto w-full z-0">
-                <Illustration className="fill-none w-full h-full dark:text-zinc-500 text-zinc-400" />
+                <Illustration className="fill-none w-full h-full dark:text-zinc-500 text-zinc-400 opacity-60" />
             </div>
             <Typography
                 size="lg"
